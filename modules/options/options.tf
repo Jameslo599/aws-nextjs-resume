b7d99a6b9853fc37b0832097000a41a8473d208e
James Lo <jameslo599@gmail.com>
2025-01-25 01:43:32 -0600

    IaC CORS"

--- modules/options/options.tf
+++ modules/options/options.tf
@@ -3,51 +3,59 @@
 variable "rest_api_id" {}
 variable "resource_id" {}
 
-resource "aws_api_gateway_method" "options" {
-  rest_api_id      = var.rest_api_id
-  resource_id      = var.resource_id
-  http_method      = "OPTIONS"
-  authorization    = "NONE"
-  api_key_required = false
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = var.rest_api_id
+  resource_id   = var.resource_id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
 }
 
-# OPTIONS method response.
-resource "aws_api_gateway_method_response" "options" {
-  rest_api_id      = var.rest_api_id
-  resource_id      = var.resource_id
-  http_method = aws_api_gateway_method.options.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
+# Create the Integration for the OPTIONS method (MOCK integration)
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id   = var.rest_api_id
+  resource_id   = var.resource_id
+  http_method   = aws_api_gateway_method.options_method.http_method
+  type          = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
   }
+
+  integration_responses {
+    status_code = "200"
+
+    response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+      "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'",
+      "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    }
+  }
+}
+
+# Create the Method Response for the OPTIONS method
+resource "aws_api_gateway_method_response" "options_method_response" {
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = "200"
+
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
     "method.response.header.Access-Control-Allow-Origin"  = true
   }
 }
 
-# OPTIONS integration.
-resource "aws_api_gateway_integration" "options" {
-  rest_api_id      = var.rest_api_id
-  resource_id      = var.resource_id
-  http_method          = "OPTIONS"
-  type                 = "MOCK"
-  passthrough_behavior = "WHEN_NO_MATCH"
-  request_templates = {
-    "application/json" : "{\"statusCode\": 200}"
-  }
-}
+# Attach the integration response to the method response
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id             = var.rest_api_id
+  resource_id             = var.resource_id
+  http_method             = aws_api_gateway_method.options_method.http_method
+  status_code            = "200"
 
-# OPTIONS integration response.
-resource "aws_api_gateway_integration_response" "options" {
-  rest_api_id      = var.rest_api_id
-  resource_id      = var.resource_id
-  http_method = aws_api_gateway_integration.options.http_method
-  status_code = "200"
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 }
