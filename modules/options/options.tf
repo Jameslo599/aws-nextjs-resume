99ab146eff1975d04cbad4c59f83f75d5ce53866
James Lo <jameslo599@gmail.com>
2025-01-25 01:27:08 -0600

    IaC CORS"

--- modules/options/options.tf
+++ modules/options/options.tf
@@ -4,47 +4,31 @@ variable "rest_api_id" {}
 variable "resource_id" {}
 
 resource "aws_api_gateway_method" "options_method" {
-  rest_api_id  = var.rest_api_id
-  resource_id  = var.resource_id
-  http_method  = "OPTIONS"
+  rest_api_id   = var.rest_api_id
+  resource_id   = var.resource_id
+  http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_response" "options_method_response" {
-  rest_api_id = var.rest_api_id
-  resource_id = var.resource_id
-  http_method = aws_api_gateway_method.options_method.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-}
-
-# OPTIONS integration.
 resource "aws_api_gateway_integration" "options_integration" {
-  rest_api_id  = var.rest_api_id
-  resource_id  = var.resource_id
-  http_method  = "OPTIONS"
-  type = "MOCK"
-  passthrough_behavior = "WHEN_NO_MATCH"
+  rest_api_id   = var.rest_api_id
+  resource_id   = var.resource_id
+  http_method   = aws_api_gateway_method.options_method.http_method
+  type          = "MOCK"
+
   request_templates = {
-    "application/json" : "{\"statusCode\": 200}"
+    "application/json" = "{\"statusCode\": 200}"
   }
 }
 
 resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = var.rest_api_id
-  resource_id = var.resource_id
-  http_method = aws_api_gateway_method.options_method.http_method
-  status_code = "200"
+  rest_api_id   = var.rest_api_id
+  resource_id   = var.resource_id
+  http_method   = aws_api_gateway_method.options_method.http_method
+  status_code   = "200"
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Api-Key'",
     "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
