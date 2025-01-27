9cd97a3a09c21bbc6aab5c5070d038f06cdd6341
James Lo <73731156+Jameslo599@users.noreply.github.com>
2025-01-27 00:38:30 -0600

    implement custom lambda verioning

--- lambda.tf
+++ lambda.tf
@@ -1,33 +1,36 @@
 # Create the Lambda function
+variable "getCounter_zip" {
+      type = string
+      description = "..."
+}
 resource "aws_lambda_function" "terraGetCounter" {
   function_name = "terraGetCounter"
   runtime       = "python3.9"
   handler       = "terraGetCounter.lambda_handler"
   role          = "arn:aws:iam::503561410637:role/lambda-dynamodb"
-  filename      = "terraGetCounter.zip"
-
-  # Set the source for the deployment package (e.g., path or S3 bucket)
-  source_code_hash = filebase64sha256("terraGetCounter.zip")
+  filename      = "${var.getCounter_zip}"
 }
 
+variable "checkUnique_zip" {
+      type = string
+      description = "..."
+}
 resource "aws_lambda_function" "terraCheckUnique" {
   function_name = "terraCheckUnique"
   runtime       = "python3.9"
   handler       = "terraCheckUnique.lambda_handler"
   role          = "arn:aws:iam::503561410637:role/lambda-dynamodb"
-  filename      = "terraCheckUnique.zip"
-
-  # Set the source for the deployment package (e.g., path or S3 bucket)
-  source_code_hash = filebase64sha256("terraCheckUnique.zip")
+  filename      = "${var.checkUnique_zip}"
 }
 
+variable "increment_zip" {
+      type = string
+      description = "..."
+}
 resource "aws_lambda_function" "terraIncrementCounter" {
   function_name = "terraIncrementCounter"
   runtime       = "python3.9"
   handler       = "terraIncrementCounter.lambda_handler"
   role          = "arn:aws:iam::503561410637:role/lambda-dynamodb"
-  filename      = "terraIncrementCounter.zip"
-
-  # Set the source for the deployment package (e.g., path or S3 bucket)
-  source_code_hash = filebase64sha256("terraIncrementCounter.zip")
+  filename      = "${var.increment_zip}"
 }
